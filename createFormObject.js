7180911e8039c59cd9bcb4f8d0c3eceeba80003a
samps <sampsons79@gmail.com>
2022-02-16 18:35:30 -0500

    init commit

---
+++ createFormObject.js
@@ -0,0 +1,10 @@
+let createForm = {
+    credits: {
+        initial: { name: Shawn Sampson, contribution: 'initial creation', },
+    },
+    readme: 'Readme file on how to utilize the form creator',
+    createShell: 'function to create a shell to contain the form',
+    labelMaker: 'function to create form labels for each input.',
+    inputMaker: 'to be a function on creating form inputs that can be created this way',
+
+}
